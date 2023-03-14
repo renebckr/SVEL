15b74241a87af300a281f03d794a1460aa39235a
René Pascal Becker <rene.becker2@gmx.de>
2023-03-14 14:27:32 +0100

    [FTR] Added Shader

---
+++ SVEL/include/svel/detail/shader.h
@@ -0,0 +1,16 @@
+#ifndef __SVEL_DETAIL_SHADER_H__
+#define __SVEL_DETAIL_SHADER_H__
+
+#include <svel/config.h>
+
+#include <memory>
+
+namespace SVEL_NAMESPACE {
+
+class IShader {
+  SVEL_PIMPL
+};
+
+} // namespace SVEL_NAMESPACE
+
+#endif /* __SVEL_DETAIL_SHADER_H__ */
