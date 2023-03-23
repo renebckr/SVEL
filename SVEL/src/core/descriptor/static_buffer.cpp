781be7be444a42409ea9d69f36334e25a7415473
René Pascal Becker <rene.becker2@gmx.de>
2023-03-23 19:54:28 +0100

    [DOC] Document StaticBuffer

--- SVEL/src/core/descriptor/static_buffer.cpp
+++ SVEL/src/core/descriptor/static_buffer.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file static_buffer.cpp
+ * @author René Pascal Becker (rene.becker2@gmx.de)
+ * @brief Implementation of the static buffer.
+ * @date 2023-03-23
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+// Local
 #include "static_buffer.h"
 
 using namespace core::descriptor;
@@ -5,13 +16,17 @@ using namespace core::descriptor;
 StaticBuffer::StaticBuffer(core::SharedDevice device,
                            vk::DescriptorType descriptorType, size_t size)
     : _device(device), _size(size), _descriptorType(descriptorType) {
-
+  // Create the buffer
   _buffer = std::make_unique<core::Buffer>(
       _device, size, _getUsage(_descriptorType),
       vk::MemoryPropertyFlagBits::eHostVisible |
           vk::MemoryPropertyFlagBits::eHostCoherent);
+
+  // Get the memory pointer from the buffer.
   _memoryPointer = device->AsVulkanObj().mapMemory(
       _buffer->GetMemory(), 0, size, vk::MemoryMapFlagBits());
+
+  // Update the buffer info of the base class
   _bufferInfo =
       vk::DescriptorBufferInfo(_buffer->AsVulkanObj(), vk::DeviceSize(0), size);
 }
@@ -23,6 +38,7 @@ StaticBuffer::~StaticBuffer() {
 vk::DescriptorType StaticBuffer::GetType() const { return _descriptorType; }
 
 StaticBuffer::WriteResult StaticBuffer::Write(void *_data) {
+  // Simply copy to the buffer
   std::memcpy(_memoryPointer, _data, _size);
   return WriteResult::eSuccess;
 }
--- SVEL/src/core/descriptor/static_buffer.h
+++ SVEL/src/core/descriptor/static_buffer.h
@@ -1,27 +1,91 @@
+/**
+ * @file static_buffer.h
+ * @author René Pascal Becker (rene.becker2@gmx.de)
+ * @brief Declaration of the StaticBuffer.
+ * @date 2023-03-23
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #ifndef __CORE_DESCRIPTOR_STATIC_BUFFER_H__
 #define __CORE_DESCRIPTOR_STATIC_BUFFER_H__
 
+// Local
 #include "buffer.h"
-#include "core/device.h"
-#include "core/memory/buffer.h"
+
+// Internal
+#include <core/device.h>
+#include <core/memory/buffer.h>
 
 namespace core::descriptor {
 
+/**
+ * @brief Implementation of the Buffer Interface. This buffer only allows for
+ * one write. It should thus be regarded as static in the context of a frame.
+ */
 class StaticBuffer final : public IBuffer {
 private:
+  /**
+   * @brief Device to use.
+   */
   core::SharedDevice _device;
+
+  /**
+   * @brief The buffer that will be used.
+   */
   core::UniqueBuffer _buffer;
+
+  /**
+   * @brief The size of the buffer in bytes.
+   */
   size_t _size;
+
+  /**
+   * @brief Type of the buffer.
+   */
   vk::DescriptorType _descriptorType;
+
+  /**
+   * @brief Direct pointer to the mapped memory of the buffer.
+   */
   void *_memoryPointer;
 
 public:
+  /**
+   * @brief Construct a Static Buffer.
+   *
+   * @param device          Device to use.
+   * @param descriptorType  Type of the buffer.
+   * @param size            Size of the buffer in bytes.
+   */
   StaticBuffer(core::SharedDevice device, vk::DescriptorType descriptorType,
                size_t size);
+
+  /**
+   * @brief Destroy the Static Buffer
+   */
   ~StaticBuffer();
 
+  /**
+   * @brief Getter for the type of the buffer.
+   *
+   * @return vk::DescriptorType Type of the buffer.
+   */
   vk::DescriptorType GetType() const override;
+
+  /**
+   * @brief Writes the data to the buffer. Will overwrite anything that was
+   * written before.
+   *
+   * @param _data        Data to write.
+   * @return WriteResult Always Success.
+   */
   WriteResult Write(void *_data) override;
+
+  /**
+   * @brief Does nothing for a static buffer as every write resets the buffer.
+   */
   void Reset() override;
 };
 
